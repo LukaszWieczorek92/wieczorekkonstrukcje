8e2d02c0de11c4155799ae3af0533264dfd0cf08
Łukasz Wieczorek <wieczorek.web@gmail.com>
2024-02-15 13:24:27 +0100

    header opacity

--- jq.js
+++ jq.js
@@ -45,3 +45,15 @@ $(document).ready(function () {
     //$('.textbox').slideDown(1000);
 });
  
+
+//hiding section
+$(document).on('scroll', function () {
+    var scrollPos = $(document).scrollTop(); //jak daleko przewineliśmy
+    var sectionOffset = $(".about-us").offset().top; //gdzie jest dany element, na jakiej wysokoscio od początku strony się zaczyna.
+    //console.log(scrollPos);
+    //console.log(sectionOffset);
+
+    if (scrollPos < sectionOffset) {
+      $(".header").css('opacity', 1 - 1.2 * scrollPos / sectionOffset);
+    }
+  });
